0eeaa328c890f442bd772877213aaa872a36dff0
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-12 20:44:02 -0300

    put com fetch

--- Fetch-Discover-API/main.js
+++ Fetch-Discover-API/main.js
@@ -1,5 +1,16 @@
 const url = "http://localhost:5500/api";
 
+const newUser = {
+    name: "Olivia Zarski",
+    avatar: "http://lorempixel.com.br/400/200",
+    city: "São Paulo"
+}
+const updatedUser = {
+    name: "João Batista",
+    avatar: "http://lorempixel.com.br/400/200",
+    city: "Curitiba"
+}
+
 function getUsers() {
     fetch(url)
         .then(response => (response.json()))
@@ -7,8 +18,8 @@ function getUsers() {
         .catch(error => console.log(error))
 }
 
-function getUser() {
-    fetch(`${url}/1`)
+function getUser(id) {
+    fetch(`${url}/${id}`)
         .then(response => (response.json()))
         .then(data => {
             userName.textContent = data.name;
@@ -33,12 +44,25 @@ function addUser(newUser) {
         )
 }
 
-const newUser = {
-    name: "Olivia Zarski",
-    avatar: "http://lorempixel.com.br/400/200",
-    city: "São Paulo"
+
+//PUT
+function updateUser(updatedUser, id) {
+    fetch(`${url}/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updatedUser),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => (response.json()))
+        .then(data => alertAPI.textContent = data)
+        .catch(error => console.log(error))
 }
+
+
+
 
 addUser(newUser);
 getUsers();
-getUser();
+getUser(6);
+updateUser(updatedUser, 6);
