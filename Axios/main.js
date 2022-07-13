626fe086a6a26a3f409c8b8e4e04a6d4cc401cc9
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-07-12 21:37:07 -0300

    get com axios

--- Axios/main.js
+++ Axios/main.js
@@ -1,5 +1,10 @@
 const url = "http://localhost:5500/api";
 
+const newUser = {
+    name: "Valéria Valença",
+    avatar: "http://picsum.com.br/200/300",
+    city: "Santos"
+};
 
 //GET
 function getUsers() {
@@ -9,5 +14,30 @@ function getUsers() {
         })
         .catch(error => console.error(error));
 }
+//POST = envia para a API e retorna
+function addNewUser(newUser) {
+
+    axios.post(url, newUser)
+
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => console.error(error));
+}
+//GET = busca um usuário específico
+function getUser(id) {
+    axios.get(`${url}/${id}`)
+        .then(response => {
+
+            userName.textContent = response.data.name;
+            userCity.textContent = response.data.city;
+            userID.textContent = response.data.id;
+            userAvatar.src = response.data.avatar;
+        })
+        .catch(error => console.error(error));
+}
 
 getUsers();
+getUser(2);
+//addNewUser(newUser);
+
